a0a20634b41ea1f41734893a22065b4bb4ea9f56
EfrenBarCast <name@email.comgit config --global user.email>
2026-07-03 09:55:22 -0500

    Modificación de sección experiencia

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -2,29 +2,202 @@ function Experience() {
   return (
     <section
       id="experience"
-      className="min-h-screen flex items-center justify-center"
+      className="min-h-screen flex items-center justify-center py-20 px-6"
     >
-      <h2 className="text-5xl font-bold">
-        Experience
-      </h2>
+      <div className="max-w-7xl w-full">
+        <h2 className="text-5xl font-bold text-center text-white mb-16">
+          Experiencia <span className="text-green-500">Profesional</span>
+        </h2>
 
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Instituto Humboldt
+            </h3>
 
-
-<a href="#" class="bg-neutral-primary-soft block max-w-sm p-6 border-white border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
-    <h5 class="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Noteworthy technology acquisitions 2021</h5>
-    <p class="text-body">Here are the biggest technology acquisitions of 2025 so far, in reverse chronological order.</p>
-</a>
-
-
-<a href="#" class="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
-    <h5 class="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Noteworthy technology acquisitions 2021</h5>
-    <p class="text-body">Here are the biggest technology acquisitions of 2025 so far, in reverse chronological order.</p>
-</a>
-
-<a href="#" class="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
-    <h5 class="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Noteworthy technology acquisitions 2021</h5>
-    <p class="text-body">Here are the biggest technology acquisitions of 2025 so far, in reverse chronological order.</p>
-</a>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Helena Olaya
+              <br />
+              <strong>Cargo:</strong> Diseñador Web UI/UX
+              <br />
+              Prototipado y maquetación.
+              <br />
+              <strong>Periodo:</strong> 02 de diciembre de 2024 - 01 de octubre.
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Instituto Humboldt
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Helena Olaya
+              <br />
+              <strong>Cargo:</strong> Diseñador Web UI/UX
+              <br />
+              Prototipado y maquetación.
+              <br />
+              <strong>Periodo:</strong> 02 de diciembre de 2024 - 01 de octubre.
+              <strong>Telefono:</strong>316 692 16 62
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Universidad del Rosario
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Ismael Iriarte
+              <br />
+              <strong>Cargo:</strong> diseñador web UI/UX y maquetador Drupal
+              <br />
+              <strong>Trabaje:</strong> desde el 19 de agosto del 2019, trabajo
+              actual.
+              <strong>Telefono:</strong>312 589 92 61
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              AuditBrain
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Cesar Betancourth
+              <br />
+              <strong>Cargo:</strong> diseñador web y maquetador Wordpress
+              Prestashop
+              <br />
+              <strong>Trabaje:</strong> desde el 3 de agosto del 2018 hasta el
+              30 septiembre del 2018 <strong>Telefono:</strong> Tel: 755 27 55
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Sainet Ingeniería
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Freddy Sanabria
+              <br />
+              <strong>Cargo:</strong> diseñador web y maquetador Wordpress
+              Prestashop
+              <br />
+              <strong>Trabaje:</strong> desde el 3 de agosto del 2018 hasta el
+              30 septiembre del 2018 <strong>Telefono:</strong> 755 27 55
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Novoclick.com
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Wendy Franco
+              <br />
+              <strong>Cargo:</strong> diseñador web y diseñador de campañas de
+              marketing
+              <br />
+              <strong>Trabaje:</strong> desde el 14 de octubre del 2014 hasta el
+              20 abril del 2018 <strong>Telefono:</strong> 613 08 00
+              <strong>Celular:</strong> 38413 00 / 384 13 13
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Daﬁti.com
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Andres Guerrero Acosta
+              <br />
+              <strong>Cargo:</strong> diseñador de piezas de email marketing
+              <br />
+              <strong>Trabaje:</strong> desde el 11 de junio del 2013 hasta el 2
+              agosto del 2013 <strong>Telefono:</strong> 38413 00 / 384 13 13
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Pac Gráﬁca Agencia publicitaria
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Juan Francisco Lopez
+              <br />
+              <strong>Cargo:</strong> diseñador web y maquetador
+              <br />
+              <strong>Trabaje:</strong> desde el 23 de septiembre del 2013 hasta
+              el 5 agosto del 2014 <strong>Telefono:</strong> 3112503262
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Ideea Soluciones Ltda.
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Alexander Giraldo R
+              <br />
+              <strong>Cargo:</strong> maquetador y diseñador freelance
+              <br />
+              <strong>Trabaje:</strong> desde el 5 de septiembre del 2012 hasta
+              el 10 octubre del 2013 <strong>Telefono:</strong> (571) 4321931
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Calzado Caprino - Imacal Ltda
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Juan Carlos Ruiz
+              <br />
+              <strong>Cargo:</strong> diseñador de piezas de email marketing
+              <br />
+              <strong>Trabaje:</strong> desde el 16 de julio del 2012 hasta el 3
+              junio del 2013 <strong>Telefono:</strong> 3701266 ext 116
+            </p>
+          </a>
+          <a
+            href="#"
+            className="bg-[#] border border-white rounded-xl p-6 shadow-lg hover:border-green-400 hover:-translate-y-2 transition-all duration-300"
+          >
+            <h3 className="text-2xl font-semibold text-white mb-4">
+              Directorio virtual publicitario s.a.s
+            </h3>
+            <p className="text-gray-300 leading-7">
+              <strong>Jefe:</strong> Eduardo Quinero
+              <br />
+              <strong>Cargo:</strong> diseñador y maquetador web freelance
+              <br />
+              <strong>Trabaje:</strong> desde el 11 de mayo del 2011 hasta el 3
+              marzo del 2012 <strong>Telefono:</strong> 367 7470 - 320 326 5859
+              - 301 273 9085
+            </p>
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
